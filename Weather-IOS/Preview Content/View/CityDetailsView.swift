4517f1f0128e7f7d99909da9d17059bb7da70110
Jatin Foujdar <jatin321001@gmail.com>
2025-01-04 16:35:01 +0530

    city list and city details commit

--- Weather-IOS/Preview Content/View/CityDetailsView.swift
+++ Weather-IOS/Preview Content/View/CityDetailsView.swift
@@ -1,18 +1,42 @@
-//
-//  CityDetailsView.swift
-//  Weather-IOS
-//
-//  Created by jatin foujdar on 04/01/25.
-//
-
 import SwiftUI
 
 struct CityDetailsView: View {
+    var cityName: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text(cityName)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            HStack {
+                Text("9°C")
+                    .font(.system(size: 50))
+                    .fontWeight(.semibold)
+                
+                Image(systemName: "cloud.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60, height: 60)
+                    .foregroundColor(.blue)
+            }
+
+            Text("Cloudy")
+                .font(.title2)
+                .foregroundColor(.gray)
+
+            Text("Expect scattered clouds throughout the day with temperatures ranging from 8°C to 12°C.")
+                .font(.body)
+                .multilineTextAlignment(.center)
+                .padding()
+
+            Spacer()
+        }
+        .padding()
+        .navigationTitle("Weather Details")
     }
 }
 
 #Preview {
-    CityDetailsView()
+    CityDetailsView(cityName: "Jaipur")
 }
--- Weather-IOS/Preview Content/View/CityList.swift
+++ Weather-IOS/Preview Content/View/CityList.swift
@@ -1,27 +1,35 @@
 import SwiftUI
 
 struct CityList: View {
+    var cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"]
+    
     var body: some View {
-        VStack(alignment: .leading) {
-            ForEach(0..<5, id: \.self) { index in
-                HStack {
-                    Text("City \(index + 1)")
-                    Text("\(10 + index)°C")
-                        .padding()
-
-                    Image(systemName: "cloud.fill")
-                    Spacer()
-
-                    Image(systemName: index % 2 == 0 ? "star.fill" : "star")
-                        .foregroundColor(index % 2 == 0 ? .yellow : .gray)
+        NavigationView{
+            VStack(alignment: .leading) {
+                ForEach(0..<cities.count, id: \.self) { index in
+                    NavigationLink(destination: CityDetailsView(cityName: cities[index])) {
+                        HStack {
+                            Text(cities[index])
+                            Text("\(10 + index)°C")
+                                .padding()
+                            
+                            Image(systemName: "cloud.fill")
+                            Spacer()
+                            
+                            Image(systemName: index % 2 == 0 ? "star.fill" : "star")
+                                .foregroundColor(index % 2 == 0 ? .yellow : .gray)
+                        }
+                      
+                    }
+                    .padding()
                 }
-                .padding()
             }
+            .font(.title2)
+            .fontWeight(.bold)
+            .navigationTitle("City")
         }
-        .font(.title2)
-        .fontWeight(.bold)
-    }
-}
+     }
+   }
 
 #Preview {
     CityList()
