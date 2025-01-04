25eea254dc3b9c7d84f3b9c99da897310c0b22f9
Jatin Foujdar <jatin321001@gmail.com>
2025-01-04 12:35:07 +0530

    Initial Commit

---
+++ Weather-IOS/Weather_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_IOSApp.swift
+//  Weather-IOS
+//
+//  Created by jatin foujdar on 04/01/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
